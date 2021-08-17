e084889c9e88a78d295418d5c18ae6f7b7e01fe6
hindenbug <304184+hindenbug@users.noreply.github.com>
2021-08-17 12:25:35 +0200

    Add servers to pool, get next server, load balance

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/hindenbug/toylb
 
-go 1.16
+go 1.17
 
 require github.com/labstack/echo/v4 v4.5.0 // indirect
--- main.go
+++ main.go
@@ -8,18 +8,63 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strings"
+	"sync"
+	"sync/atomic"
 )
 
-type Backend struct {
+type Server struct {
 	URL          *url.URL
 	Alive        bool
+	mux          sync.RWMutex
 	ReverseProxy *httputil.ReverseProxy
 }
 
 type ServerPool struct {
-	backends []*Backend
-	current  uint64
+	servers []*Server
+	current uint64
 }
+
+func (s *Server) IsAlive() bool {
+	return s.Alive
+}
+
+func (p *ServerPool) AddServer(server *Server) {
+	p.servers = append(p.servers, server)
+}
+
+func (p *ServerPool) AliveServerIndex() int {
+	return int(atomic.AddUint64(&p.current, uint64(1)) % uint64(len(p.servers)))
+}
+
+// get the Next alive server
+func (p *ServerPool) NextServer() *Server {
+	nextIndex := int(atomic.AddUint64(&p.current, uint64(1)))
+	l := len(p.servers) + nextIndex
+
+	for i := nextIndex; i < l; i++ {
+		next := i % len(p.servers)
+		if p.servers[next].IsAlive() {
+			if i != nextIndex {
+				atomic.StoreUint64(&p.current, uint64(next))
+			}
+			return p.servers[next]
+		}
+	}
+	return nil
+}
+
+func loadBalance(w http.ResponseWriter, r *http.Request) {
+	server := serverPool.NextServer()
+
+	if server != nil {
+		server.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+
+	http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
+}
+
+var serverPool ServerPool
 
 func main() {
 	var serverList string
@@ -35,6 +80,7 @@ func main() {
 
 	serverTokens := strings.Split(serverList, ",")
 
+	// parse servers
 	for _, token := range serverTokens {
 		serverUrl, err := url.Parse(token)
 
@@ -42,16 +88,23 @@ func main() {
 			log.Fatal(err)
 		}
 
+		// initialize reverse proxy
 		reverseProxy := httputil.NewSingleHostReverseProxy(serverUrl)
 
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", port),
-			Handler: http.HandlerFunc(reverseProxy.ServeHTTP),
-		}
-
-		log.Printf("Load Balancer started at :%d\n", port)
-		if err := server.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
+		// add server to ServerPool
+		serverPool.AddServer(&Server{URL: serverUrl, Alive: true, ReverseProxy: reverseProxy})
+		log.Printf("Configured instance: %s\n", serverUrl)
 	}
+
+	// create http server
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(loadBalance),
+	}
+
+	log.Printf("Load Balancer started at :%d\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
 }
